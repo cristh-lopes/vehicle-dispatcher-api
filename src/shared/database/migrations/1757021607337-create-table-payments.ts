2855ee7bb9516db36940e718ac5c211cdad04be5
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-09-04 18:38:04 -0300

    feat: CreateTablePayments migration

---
+++ src/shared/database/migrations/1757021607337-create-table-payments.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTablePayments1757021607337 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "tb_payments" (
+        "id" uuid NOT NULL,
+        "name" character varying NOT NULL,
+        "note" character varying NULL,
+        "amount" numeric(10,2) NOT NULL,
+        "payment_date" TIMESTAMP NOT NULL DEFAULT now(),
+        "payments_types_id" uuid NULL,
+        "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+        "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+        "deleted_at" TIMESTAMP,
+        CONSTRAINT "PK_tb_payments_id" PRIMARY KEY ("id")
+      )`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "tb_payments"`);
+  }
+}
