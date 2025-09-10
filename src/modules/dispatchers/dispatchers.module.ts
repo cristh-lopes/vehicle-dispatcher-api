14eeab6985bafe1cc3e99e79c9cc2e1e63d1d5e8
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-09-10 20:28:02 -0300

    chore: export Dispatcher entity

--- src/modules/dispatchers/dispatchers.module.ts
+++ src/modules/dispatchers/dispatchers.module.ts
@@ -5,10 +5,13 @@ import {
   dispatcherModuleImports,
   dispatcherModuleProviders,
 } from './module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dispatcher } from './database/entities/dispatcher.entity';
 
 @Module({
   imports: dispatcherModuleImports,
   controllers: dispatcherModuleControllers,
   providers: dispatcherModuleProviders,
+  exports: [TypeOrmModule.forFeature([Dispatcher])],
 })
 export class DispatchersModule {}
